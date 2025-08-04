5d7f53a80eb30f46ace2935b51e40dcc3ccb9dd1
Muhammad Sakeen <muhammadsakeen06@gmail.com>
2025-08-04 21:13:59 +0530

    Updated firebase config to use env vars

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,12 +2,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCE0oBlXlJh2Yexwve9EsfXHuZTMw7y2IA",
-  authDomain: "movie-library-app-f096c.firebaseapp.com",
-  projectId: "movie-library-app-f096c",
-  storageBucket: "movie-library-app-f096c.firebasestorage.app",
-  messagingSenderId: "891138646717",
-  appId: "1:891138646717:web:2c0c810b6a8c6b24c9cdd4"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
